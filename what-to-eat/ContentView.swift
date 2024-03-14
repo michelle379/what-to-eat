f18de1dd0dad5f829232db85a58d4e50aed42e3d
michelle379 <michelle379123@gmail.com>
2024-03-14 17:46:04 +0800

    Initial Commit

---
+++ what-to-eat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  what-to-eat
+//
+//  Created by Apple on 2024/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ what-to-eat/what_to_eatApp.swift
@@ -0,0 +1,17 @@
+//
+//  what_to_eatApp.swift
+//  what-to-eat
+//
+//  Created by Apple on 2024/3/14.
+//
+
+import SwiftUI
+
+@main
+struct what_to_eatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
